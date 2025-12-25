cacf0ab3c6e48e147f865d5756c61fa81f35c238
harry1012@foxmail.com <haoleiq@gmail.com>
2025-12-25 10:52:45 +0800

    Site updated: 2025-12-25 10:52:44

--- js/allpost_data.js
+++ js/allpost_data.js
@@ -1,20 +1,5 @@
 window.BLOG_POSTS = [
   {
-    "title": "Esay Period - Elegant Period Tracking App",
-    "path": "p/13912/",
-    "date": "2026-01-01",
-    "year": 2026,
-    "categories": [
-      "iPhone"
-    ],
-    "tags": [
-      "Health"
-    ],
-    "cover": "https://harry-studio.github.io/app/my/project/esay-period/1.jpg",
-    "description": null,
-    "excerpt": ""
-  },
-  {
     "title": "WealthTrees - Private Wealth Tracker",
     "path": "p/13913/",
     "date": "2026-01-02",
@@ -26,6 +11,21 @@ window.BLOG_POSTS = [
       "Wealth"
     ],
     "cover": "https://harry-studio.github.io/app/my/project/wealth/1.jpg",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "Esay Period - Elegant Period Tracking App",
+    "path": "p/13912/",
+    "date": "2026-01-01",
+    "year": 2026,
+    "categories": [
+      "iPhone"
+    ],
+    "tags": [
+      "Health"
+    ],
+    "cover": "https://harry-studio.github.io/app/my/project/esay-period/1.jpg",
     "description": null,
     "excerpt": ""
   }
